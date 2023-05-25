a90bf7cca159057702d4c0686989b83ecec19ebb
sandramkh516 <118845782+sandramkh516@users.noreply.github.com>
2023-05-25 21:40:19 +0430

    add github link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1> Weather app</h1>
+      <footer>
+        This project was coded by Samaneh Mokhtari and is {""}
+        <a
+          href="https://github.com/sandramkh516/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-source
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
